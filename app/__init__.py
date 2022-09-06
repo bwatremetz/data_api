5a0b3a1e75eb5485f9994b282b746cf209312f3a
benoit <benoit.watremetz@gmail.com>
2022-09-06 16:59:28 +0200

    add openapi specs

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from config import Config
 
 def create_app(config_class=Config):
     app = APIFlask(__name__, title='PowerApp', version='0.1')
+    app.config['LOCAL_SPEC_PATH'] = 'openapi.json'   # run flask spec to write the OpenAPI specification file
 
     with app.app_context():
         from app.power_model.routes import power_bp 
